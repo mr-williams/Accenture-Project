9bd8777eec2ac0bb4d570467d28728bdd6a9d60e
mr-williams <70599292+mr-williams@users.noreply.github.com>
2022-04-18 19:46:09 +0000

    Update Accenture-Project.sql

--- Accenture-Project.sql
+++ Accenture-Project.sql
@@ -1,3 +1,4 @@
+/*This Query Joins certain csv files to show the General info of the Users on Social Buzz*/
 SELECT Profile.User_ID,Name,Age, Email,Address,Interests, Device, Duration
 INTO User_info
 FROM Profile
@@ -8,5 +9,10 @@ ON Location.User_ID = Session.User_ID
 INNER JOIN Users
 ON Session.User_ID = Users.User_ID
 
+/*Thes Queries were used in the Data Cleaning Phase*/
+SELECT *
+FROM Reactions
+WHERE Type is NULL
 
+DELETE FROM Reaction WHERE Type=' ' OR Type IS NULL;
 
